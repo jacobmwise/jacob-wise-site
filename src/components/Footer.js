7b8882f288d3230b987199b6419ddcd861570aa8
Jacob Wise <jmw555@cornell.edu>
2020-06-22 21:59:51 -0400

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="bg-info">
+            <div>
+                Contact
+            </div>
+            <div>
+                Social
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
