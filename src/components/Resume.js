a5b534b52cea174f1108fcf6c01cfa37647ae56e
Jacob Wise <jmw555@cornell.edu>
2020-07-06 17:39:16 -0400

    start pdf rendering

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Document, Page, pdfjs} from 'react-pdf';
 
 import Nav from './subcomponents/Nav';
 import Footer from './subcomponents/Footer';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 class Resume extends React.Component {
   
@@ -9,6 +12,10 @@ class Resume extends React.Component {
         return (
             <div className="bg-light" style={{height: '100vh'}}>
                 <Nav />
+
+                <Document file="../files/resume.pdf">
+
+                </Document>
 
                 <Footer />
             </div>
