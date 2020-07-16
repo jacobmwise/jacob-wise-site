e1ba9ecb153cc70c535367e99525de06d592812a
Jacob Wise <jmw555@cornell.edu>
2020-07-15 21:20:36 -0400

    projects page content progress

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,18 +9,24 @@ import DisplayContent from './DisplayContent';
 const Projects = () => {
     const sections = [
         <Section
-            src={require("../images/headshot.jpeg")}
+            src={require("../images/travel.png")}
             name="Travel Site"
             title="Class Project"
             date="April 2020"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            description="For my final project in INFO 2300: Intermediate Design and Programming for the Web, I made a travel site image gallery. The site was 
+            developed using PHP, which allowed me to build on the HTML and CSS knowledge previously gained from coursework with added functionality, 
+            such as image filtering, image tags, and form components. Using SQL, I connected to an SQLite development database and retrieved info related to each 
+            image on the site. The database is also updated upon image addition/deletion.<br/><br/> A link to my site can be found 
+            <a href='https://wise-travel.herokuapp.com/' target='_blank'>here</a>. It is designed for desktop use only."
         />,
         <Section
-            src={require("../images/headshot.jpeg")}
+            src={require("../images/mlb.png")}
             name="MLB Salary Analysis"
             title="Class Project"
             date="March - April 2020"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            description="My final project prompt for INFO 2950: Introduction to Data Science was pretty open ended; we were required to use our knowledge from 
+            throughout the semester to analyze and answer a research question to our greatest ability. My group decided to use MLB statistics to answer the following: 
+            what factors and statistics lead MLB hitters to receive higher salaries?"
             reverse
         />
     ];
